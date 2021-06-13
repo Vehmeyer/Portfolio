ffdb1064d4264815262f6d092c75243d54f62603
John Vehmeyer <j.vehmeyer@gmail.com>
2021-06-12 19:57:18 -0500

    created react app for portfolio. base scaffolding in place.

---
+++ portfolio/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>JV Portfolio</h1>
+      </header>
+      <body>
+        <h2>Portfolio Project Start</h2>
+      </body>
+    </div>
+  );
+}
+
+export default App;
